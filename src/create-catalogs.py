a36dff783854facf1e5b4afdd8d8396c3e25484b
matthew-gigl-db <matthew-gigl-db>
2024-06-27 10:39:26 +0000

    created catalogs json and completed notebook for setting catalogs and granting permissions

--- src/create-catalogs.py
+++ src/create-catalogs.py
@@ -4,30 +4,60 @@ dbutils.widgets.text("fixturePath", "", "")
 
 # COMMAND ----------
 
+sourcePath = dbutils.widgets.get("sourcePath")
+fixturePath = dbutils.widgets.get("fixturePath")
+
+# COMMAND ----------
+
+print(f"""
+   sourcePath = {sourcePath}
+   fixturePath = {fixturePath}    
+""")
+
+# COMMAND ----------
+
 import sys, os
 sys.path.append(os.path.abspath(dbutils.widgets.get("sourcePath")))
 
 import ucSetUp
+import pandas as pd
 
 # COMMAND ----------
 
-catalog = dbutils.widgets.get("catalog")
-target = dbutils.widgets.get("target")
+catalogs_fixture = fixturePath + "/catalogs.json"
+df = pd.read_json(catalogs_fixture)
+sdf = spark.createDataFrame(df)
 
 # COMMAND ----------
 
-if catalog == target:
-  catalog = catalog
-elif target == "prod":
-  catalog = catalog
-else:
-  catalog = f"{catalog}_{target}"
+from pyspark.sql.functions import explode, col
+
+catalogs_sdf = (
+  sdf
+  .withColumn("name", col("catalog.name"))
+  .withColumn("permissions", explode(col("catalog.permissions")))
+  .select("name", "permissions")
+)
+
+display(catalogs_sdf)
 
 # COMMAND ----------
 
-ucSetUp.hello_world(catalog)
+catalogs_list = catalogs_sdf.collect()
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC create catalog if not exists ${catalog};
+distinct_catalogs = set([i["name"] for i in catalogs_list])
+for catalog in distinct_catalogs:
+  print(f"CREATE CATALOG IF NOT EXISTS {catalog};")
+  # spark.sql(f"CREATE CATALOG IF NOT EXISTS {catalog};")
+
+
+# COMMAND ----------
+
+for i in catalogs_list:
+  catalog = i["name"]
+  grant = i["permissions"]["grant"]
+  for principal in i["permissions"]["principals"]:
+    print(f"GRANT {grant} ON CATALOG {catalog} TO `{principal}`;")
+    # spark.sql(f"GRANT {grant} ON CATALOG {catalog} TO `{principal}`;")
